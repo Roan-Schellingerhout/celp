b0a6a3d0c28bb04518991255f522eac3e39b7ef6
Roan-Schellingerhout <roan.schellingerhout@gmail.com>
2019-05-07 17:01:12 +0200

    stad per gebruiker vinden, long/lat per stad vinden

--- recommender.py
+++ recommender.py
@@ -1,6 +1,8 @@
 from data import CITIES, BUSINESSES, USERS, REVIEWS, TIPS, CHECKINS
 
+import data
 import random
+from tqdm import tqdm
 
 def recommend(user_id=None, business_id=None, city=None, n=10):
     """
@@ -19,26 +21,49 @@ def recommend(user_id=None, business_id=None, city=None, n=10):
         city = random.choice(CITIES)
     return random.sample(BUSINESSES[city], n)
 
-def location(user_id):
-    """Finds the longitude and latitude of a user based on the locations of previously-reviewed businesses"""
+
+# def location(user_id):
+#     """Finds the longitude and latitude of a user based on the locations of previously-reviewed businesses"""
     
-    # init variables
+#     # init variables
+#     long = []
+#     lat = []
+
+#     # go over all reviews in all cities
+#     for city in tqdm(CITIES):
+#         print("a")
+#         for i in REVIEWS[city]:
+#             # if the review was left by the desired user, save the business_id
+#             if i["user_id"] == user_id:
+#                 business_id = i["business_id"]
+#                 # go through all businesses and, if it's the desired business_id, find the exact location
+#                 for j in BUSINESSES[city]:
+#                     if j["business_id"] == business_id:
+#                         long.append(j["longitude"])
+#                         lat.append(j["latitude"])
+    
+#     if len(long) == 0 or len(lat) == 0:
+#         return None
+#     # return the average latitude and longitude of the businesses
+#     return {"latitude" : sum(lat)/len(lat), "longitude" : sum(long)/len(long)}
+
+woonplaatsen = {}
+
+for city in CITIES:
+    for i in USERS[city]:
+        woonplaatsen[i["user_id"]] = city
+
+long = []
+lat = []
+locaties = {}
+
+for city in CITIES:
+    for i in BUSINESSES[city]:
+        long.append(i["longitude"])
+        lat.append(i["latitude"])
+    if long and lat:
+        locaties[city] = (sum(lat)/len(lat), sum(long)/len(long))
+    else:
+        locaties[city] = None
     long = []
     lat = []
-
-    # go over all reviews in all cities
-    for city in CITIES:
-        for i in REVIEWS[city]:
-            # if the review was left by the desired user, save the business_id
-            if i["user_id"] == user_id:
-                business_id = i["business_id"]
-                # go through all businesses and, if it's the desired business_id, find the exact location
-                for j in BUSINESSES[city]:
-                    if j["business_id"] == business_id:
-                        long.append(j["longitude"])
-                        lat.append(j["latitude"])
-    
-    # return the average latitude and longitude of the businesses
-    return {"latitude" : sum(lat)/len(lat), "longitude" : sum(long)/len(long)}
-
-print(location("1-aIDjs2uWYLcoPTDBy5kg"))
