9dc1b9b675af99bed9d1ddaf73cf4a9246338f1b
Roan-Schellingerhout <roan.schellingerhout@gmail.com>
2019-05-07 14:16:36 +0200

    location finder geïmplementeerd

--- recommender.py
+++ recommender.py
@@ -18,3 +18,26 @@ def recommend(user_id=None, business_id=None, city=None, n=10):
     if not city:
         city = random.choice(CITIES)
     return random.sample(BUSINESSES[city], n)
+
+def location(user_id):
+    """Finds the longitude and latitude of a user based on the locations of previously-reviewed businesses"""
+    
+    # init variables
+    long = []
+    lat = []
+
+    # go over all reviews in all cities
+    for city in CITIES:
+        for i in REVIEWS[city]:
+            # if the review was left by the desired user, save the business_id
+            if i["user_id"] == user_id:
+                business_id = i["business_id"]
+                # go through all businesses and find their exact location
+                for j in BUSINESSES[city]:
+                    if j["business_id"] == business_id:
+                        long.append(j["longitude"])
+                        lat.append(j["latitude"])
+    
+    return {"latitude" : sum(lat)/len(lat), "longitude" : sum(long)/len(long)}
+
+print(location("1-aIDjs2uWYLcoPTDBy5kg"))
