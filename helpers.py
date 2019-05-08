74194340301dc7681722b0c5dce0190fec780ded
Roan-Schellingerhout <roan.schellingerhout@gmail.com>
2019-05-08 13:43:52 +0200

    location_user weer aangemaakt. ongeveer 5000 it/s aldus tqdm

--- helpers.py
+++ helpers.py
@@ -58,26 +58,28 @@ def location(city):
         return None
 
 
-# def location_user(user_id):
-#     """Finds the longitude and latitude of a user based on the locations of previously-reviewed businesses"""
+def location_user(user_id):
+    """Finds the longitude and latitude of a user based on the locations of previously-reviewed businesses"""
     
-#     # init variables
-#     long = []
-#     lat = []
+    # init variables
+    long = []
+    lat = []
 
-#     # go over all reviews in all cities
-#     for city in tqdm(CITIES):
-#         for i in REVIEWS[city]:
-#             # if the review was left by the desired user, save the business_id
-#             if i["user_id"] == user_id:
-#                 business_id = i["business_id"]
-#                 # go through all businesses and, if it's the desired business_id, find the exact location
-#                 for j in BUSINESSES[city]:
-#                     if j["business_id"] == business_id:
-#                         long.append(j["longitude"])
-#                         lat.append(j["latitude"])
+    # go over all reviews in all cities
+    for city in tqdm(CITIES):
+        for i in REVIEWS[city]:
+            # if the review was left by the desired user, save the business_id
+            if i["user_id"] == user_id:
+                business_id = i["business_id"]
+                # go through all businesses and, if it's the desired business_id, find the exact location
+                for j in BUSINESSES[city]:
+                    if j["business_id"] == business_id:
+                        long.append(j["longitude"])
+                        lat.append(j["latitude"])
     
-#     if len(long) == 0 or len(lat) == 0:
-#         return None
-#     # return the average latitude and longitude of the businesses
-#     return {"latitude" : sum(lat)/len(lat), "longitude" : sum(long)/len(long)}
+    if len(long) == 0 or len(lat) == 0:
+        return None
+    # return the average latitude and longitude of the businesses
+    return {"latitude" : sum(lat)/len(lat), "longitude" : sum(long)/len(long)}
+
+print(location_user("-S1dz92Q3RPfHomiqEeP8Q"))
