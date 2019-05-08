7ed55bb319edaaf1c5ec54702fa685f0c9826415
Roan-Schellingerhout <roan.schellingerhout@gmail.com>
2019-05-08 13:59:02 +0200

    roan up-to-date met master

--- helpers.py
+++ helpers.py
@@ -6,13 +6,13 @@ import random
 from tqdm import tqdm
 
 
-def distance(city1, city2):
-    """Calculates the distance between two longitude latitude pairs in km"""
+def distance(user_id, business_id):
+    """Calculates the distance between two a user and a business in km"""
     # radius of earth in km 
     R = 6373.0
 
-    loc1 = location(city1)
-    loc2 = location(city2)
+    loc1 = location_user(user_id)
+    loc2 = location_business(business_id)
 
     # convert degrees to radians
     lat1 = radians(loc1[0]) 
@@ -20,10 +20,7 @@ def distance(city1, city2):
     lat2 = radians(loc2[0]) 
     lon2 = radians(loc2[1]) 
 
-    dlon = lon2 - lon1
-    dlat = lat2 - lat1 
-
-    a = sin(dlat / 2)**2 + cos(lat1) * cos(lat2) * sin(dlon / 2)**2 
+    a = sin((lat2 - lat1) / 2)**2 + cos(lat1) * cos(lat2) * sin((lon2 - lon1) / 2)**2 
     c = 2 * atan2(sqrt(a), sqrt(1 - a)) 
 
     return R * c
@@ -41,7 +38,7 @@ def hometowns():
     return hometowns
     
 
-def location(city):
+def location_city(city):
     """Return a tuple with (latitude, longitude) for the city""" 
     # init variables
     lon = []
@@ -56,27 +53,38 @@ def location(city):
         return (sum(lat)/len(lat), sum(lon)/len(lon))
     else:
         return None
-        
-# def location_user(user_id):
-#     """Finds the longitude and latitude of a user based on the locations of previously-reviewed businesses"""
-    
-#     # init variables
-#     long = []
-#     lat = []
 
-#     # go over all reviews in all cities
-#     for city in tqdm(CITIES):
-#         for i in REVIEWS[city]:
-#             # if the review was left by the desired user, save the business_id
-#             if i["user_id"] == user_id:
-#                 business_id = i["business_id"]
-#                 # go through all businesses and, if it's the desired business_id, find the exact location
-#                 for j in BUSINESSES[city]:
-#                     if j["business_id"] == business_id:
-#                         long.append(j["longitude"])
-#                         lat.append(j["latitude"])
+
+def location_user(user_id):
+    """Finds the longitude and latitude of a user based on the locations of previously-reviewed businesses"""
     
-#     if len(long) == 0 or len(lat) == 0:
-#         return None
-#     # return the average latitude and longitude of the businesses
-#     return {"latitude" : sum(lat)/len(lat), "longitude" : sum(long)/len(long)}
+    # init variables
+    long = []
+    lat = []
+
+    # go over all reviews in all cities
+    for city in CITIES:
+        for i in REVIEWS[city]:
+            # if the review was left by the desired user, save the business_id
+            if i["user_id"] == user_id:
+                business_id = i["business_id"]
+                # go through all businesses and, if it's the desired business_id, find the exact location
+                for j in BUSINESSES[city]:
+                    if j["business_id"] == business_id:
+                        long.append(j["longitude"])
+                        lat.append(j["latitude"])
+    
+    if len(long) == 0 or len(lat) == 0:
+        return None
+    # return the average latitude and longitude of the businesses
+    return (sum(lat)/len(lat), sum(long)/len(long))
+
+
+def location_business(business_id):
+    for city in CITIES:
+        for business in BUSINESSES[city]:
+            if business["business_id"] == business_id:
+                return (business["latitude"], business["longitude"])
+
+
+print(distance("-S1dz92Q3RPfHomiqEeP8Q", "p8u03IURLPg_5jYSY3OmMQ"))
