f6b63af8129dd940844f415dcba77e656d57cfe6
Roan-Schellingerhout <roan.schellingerhout@student.uva.nl>
2019-05-16 14:13:00 +0200

    functie die een test set compleet vult met predictions

--- helpers.py
+++ helpers.py
@@ -301,7 +301,6 @@ def test_mse(neighborhood_size = 5, filtertype="collaborative filtering"):
     all_df = json_to_df()
     df = split_data(all_df)
     ut = create_utility_matrix(df[0])
-    print(ut)
 
     if filtertype == "collaborative filtering":
         print("Creating needed variables...")
@@ -322,10 +321,54 @@ def test_mse(neighborhood_size = 5, filtertype="collaborative filtering"):
     print("Starting calculations...")
     mses = {}
     # test the mse based on the length of the neighborhood
-    for i in tqdm(range(1, neighborhood_size + 1)):     
+    for i in range(1, neighborhood_size + 1):     
         predictions = predict_ratings(sim, ut, df[1], i).dropna()
-        predictions = predictions[predictions["predicted rating"] > 4.5]
         mses[i] = mse(predictions)
+    return mses
+
+# test_mse(filtertype="content based")
+# test_mse(filtertype="spacy")
+# test_mse()
+
+
+def predict_all():
+    """Fills an entire test set with predictions"""
+    
+    mses = []
+    
+    # predict cf based
+    all_df = json_to_df()
+    df = split_data(all_df)
+    ut = create_utility_matrix(df[0])
+    sim = similarity_matrix_cosine(ut)
+    predictions = predict_ratings(sim, ut, df[1], 0)
+    mses.append(mse(predictions))
+
+    # find which values are still np.nan
+    to_predict = predictions.loc[~predictions.index.isin(predictions.dropna().index)]
+    
+    # predict content-based (normal) for those rows
+    cats = json_to_df_categories()
+    fancy_cats = extract_genres(cats)
+    ut_cats = pivot_genres(fancy_cats)
+    sim = create_similarity_matrix_categories(ut_cats)
+    
+    predictions = predictions.append(predict_ratings(sim, ut, to_predict, 0))
+    mses.append(mse(predictions))
+
+    # find which values are still np.nan
+    to_predict = predictions.loc[~predictions.index.isin(predictions.dropna().index)]
+
+    # predict content-based (spacy) for those rows
+    sim = pd.read_msgpack("spacy_similarity.msgpack")
+    predictions = predictions.append(predict_ratings(sim, ut, to_predict, 0))
+    to_predict = predictions.loc[~predictions.index.isin(predictions.dropna().index)]
+    mses.append(mse(predictions))
+
+    # for the rows which have no neighborhood in any of the methods, predict the average rating of the test set
+    predictions = predictions.fillna(predictions["stars"].mean())
+    mses.append(mse(predictions))
+
     return mses
 
 
@@ -334,24 +377,25 @@ def test_mse(neighborhood_size = 5, filtertype="collaborative filtering"):
 
 Data based on following cities: clairton, claremont, clark, clarkson, cleveland, cleveland heigh, cleveland height, clevelant heights, sun city, westlake
 """
+# import matplotlib.pyplot as plt
+# import matplotlib.lines as mlines
+
 # neighborhood_len = [i+1 for i in range(10)]
 # mse = [1.7564182528686922, 1.3721995028085512, 1.197445599162912, 1.0903723452322052, 1.0330697173998888, 0.9897513505503943, 0.9468272073142202, 0.9267952685422991, 0.9139840329554876, 0.8908426363903116]
-# amount_of_predictions = [16566, 12114, 9735, 8194, 7110, 6222, 5558, 4986, 4512, 4100]
+# mse_2 = [1.8679413022514073, 1.4882743924604758, 1.3305999730163216, 1.235784939493885, 1.1872344529077588, 1.1456269013936535, 1.1165820327106752, 1.0889028676424142, 1.0717952556124921, 1.0582349169023897]
+# mse_3 = [2.3441104860640687, 1.79612055000979, 1.5573728644495473, 1.4420303560817995, 1.2950954895714613, 1.2682969141074594, 1.2662363564547128, 1.2908763865812998, 1.273829947371037, 1.3300483826754819]
 
-# fig, ax1 = plt.subplots()
+# plt.xlabel("Minimum length of neighborhood")
+# plt.ylabel("Mean squared error")
+# plt.plot(neighborhood_len, mse, color="red")
+# mse_cf = mlines.Line2D([], [], color='red', label='mse (item-based collaborative filtering)')
 
-# ax1.set_xlabel("Minimum length of neighborhood")
-# ax1.set_ylabel("Mean squared error")
-# ax1.plot(neighborhood_len, mse, color="red")
-# mse = mlines.Line2D([], [], color='red', label='Mean squared error')
+# plt.plot(neighborhood_len, mse_2, color="blue")
+# mse_cb = mlines.Line2D([], [], color='blue', label='mse (content-based filtering)')
 
-# ax2 = plt.twinx()
+# plt.plot(neighborhood_len, mse_3, color="green")
+# mse_cb_spacy = mlines.Line2D([], [], color='green', label='mse (content-based filtering with spacy)')
 
-# ax2.set_ylabel("Amount of predictions")
-# ax2.plot(neighborhood_len, amount_of_predictions, color="blue")
-# ax2.tick_params(axis="y")
-# amount = mlines.Line2D([], [], color='blue', label='Amount of predictions')
 
-# fig.tight_layout()
-# plt.legend(handles=[mse, amount], loc="upper right")
+# plt.legend(handles=[mse_cf, mse_cb, mse_cb_spacy], loc="best")
 # plt.show()
